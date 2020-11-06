0040d3504979335c0e68e6b5b44ab6f7fbcf0ede
A00886105 <95jvt71@gmail.com>
2020-11-06 15:48:40 -0800

    revert back to original

--- processing/app.py
+++ processing/app.py
@@ -98,37 +98,35 @@ def populate_stats():
     get_international_baggages = requests.get('{}/baggage/international'.format(app_config['eventstore']['url']), params=current_time)
     
     # logs for domestic baggages list
-    # if (get_domestic_baggages.status_code != 200):
-    #     logger.error('Could not receive events GET request domestic baggages list with status code {}'.format(
-    #         get_domestic_baggages.status_code))
+    if (get_domestic_baggages.status_code != 200):
+        logger.error('Could not receive events GET request domestic baggages list with status code {}'.format(
+            get_domestic_baggages.status_code))
         
-    # else:
-    try:
+    else:
         logger.info('{} events received from domestic baggages GET request with status code {}'.format(
         len(get_domestic_baggages.json()),
         get_domestic_baggages.status_code))  
         
         stats_info["num_domestic_baggages"] = stats_info["num_domestic_baggages"] + len(get_domestic_baggages.json())
-    except:
-        logger.error('Could not receive events GET request domestic baggages list with status code {}'.format(
-            get_domestic_baggages.status_code))
+    # except:
+    #     logger.error('Could not receive events GET request domestic baggages list with status code {}'.format(
+    #         get_domestic_baggages.status_code))
     
     # logs for international baggages list
-    # if (get_international_baggages.status_code != 200):
-    #     logger.error('Could not receive events GET request international baggages list with status code {}'.format(
-    #         get_international_baggages.status_code
-    #     ))
-    # else:
-    try:
+    if (get_international_baggages.status_code != 200):
+        logger.error('Could not receive events GET request international baggages list with status code {}'.format(
+            get_international_baggages.status_code
+        ))
+    else:
         logger.info('{} events received from international baggages GET request with status code {}'.format(
         len(get_international_baggages.json()),
         get_international_baggages.status_code))
         
         stats_info["num_international_baggages"] = stats_info["num_international_baggages"] + len(get_international_baggages.json())
-    except:
-        logger.error('Could not receive events GET request international baggages list with status code {}'.format(
-            get_international_baggages.status_code
-        ))
+    # except:
+    #     logger.error('Could not receive events GET request international baggages list with status code {}'.format(
+    #         get_international_baggages.status_code
+    #     ))
         
     # Updating data.json file
     stats_info["total_baggages"] = stats_info["num_domestic_baggages"] + stats_info["num_international_baggages"]
