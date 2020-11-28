a2e71d559d956a251d43b8ab4a88957aa5012920
A00886105 <95jvt71@gmail.com>
2020-11-28 10:49:48 -0800

    added more debugging

--- processing/app.py
+++ processing/app.py
@@ -127,6 +127,7 @@ def populate_stats():
     json_domestic_baggages = get_domestic_baggages.json()
     
     logger.info(json_domestic_baggages.status_code)
+    logger.info("Domestic: {}".format(json_domestic_baggages))
     
     
     # logger.info('{} events received from international baggages GET request with status code {}'.format(
